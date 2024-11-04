64fb7dbc0dce3d18e9cb790641a486846f2a2963
unknown <mdamir4298@gmail.com>
2024-11-05 00:18:23 +0530

    image uploaded and display in a list

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -1,28 +1,107 @@
 // import React from 'react'
 import "./AddProduct.css";
 import upload_area from "../../assets/Admin_Assets/upload_area.svg";
+import { useState } from "react";
 
 const AddProduct = () => {
+  const [image, setImage] = useState(false);
+  const [productDetails, setProductDetails] = useState({
+    name: "",
+    image: "",
+    category: "women",
+    new_price: "",
+    old_price: "",
+  });
+
+  const imageHandler = (e) => {
+    setImage(e.target.files[0]);
+  };
+
+  const chnageHandler = (e) => {
+    setProductDetails({ ...productDetails, [e.target.name]: e.target.value });
+  };
+
+  const Add_Product = async () => {
+    console.log(productDetails);
+    let responseData;
+    let product = productDetails;
+    let formData = new FormData();
+    formData.append("productImage", image);
+
+    await fetch("http://localhost:4000/upload", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+      body: formData,
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        responseData = data;
+      });
+
+    if (responseData.success) {
+      product.image = responseData.image_url;
+      console.log(product);
+      await fetch("http://localhost:4000/addproduct", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(product),
+      })
+        .then((resp) => resp.json())
+        .then((data) => {
+          data.success ? alert("Product addedd") : alert("Failed");
+        });
+    }
+  };
+
   return (
     <div className="add_product">
       <div className="addproduct_itemfield">
         <p>Product title</p>
-        <input type="text" name="name" placeholder="Type here" />
+        <input
+          value={productDetails.name}
+          onChange={chnageHandler}
+          type="text"
+          name="name"
+          placeholder="Type here"
+        />
       </div>
       <div className="addproduct_price">
         <div className="addproduct_itemfield">
           <p>Price</p>
-          <input type="text" name="old_price" placeholder="Type here" />
+          <input
+            value={productDetails.old_price}
+            onChange={chnageHandler}
+            type="text"
+            name="old_price"
+            placeholder="Type here"
+          />
         </div>
         <div className="addproduct_itemfield">
           <p>Offer Price</p>
-          <input type="text" name="new_price" placeholder="Type here" />
+          <input
+            value={productDetails.new_price}
+            onChange={chnageHandler}
+            type="text"
+            name="new_price"
+            placeholder="Type here"
+          />
         </div>
       </div>
 
       <div className="addproduct-itemfield">
         <p>Product Category</p>
-        <select name="category" id="" className="add_product_selector">
+        <select
+          value={productDetails.category}
+          onChange={chnageHandler}
+          name="category"
+          id=""
+          className="add_product_selector"
+        >
           <option value="women">Women</option>
           <option value="men">Men</option>
           <option value="kid">Kid</option>
@@ -30,11 +109,28 @@ const AddProduct = () => {
       </div>
       <div className="addproduct_itemfield">
         <label htmlFor="file-input">
-          <img src={upload_area} alt="" className="addproduct-thumbnail-img" />
+          <img
+            src={image ? URL.createObjectURL(image) : upload_area}
+            alt=""
+            className="addproduct-thumbnail-img"
+          />
         </label>
-        <input type="file" name="profileImage" id="file-input" hidden />
+        <input
+          onChange={imageHandler}
+          type="file"
+          name="profileImage"
+          id="file-input"
+          hidden
+        />
       </div>
-      <button className="addproduct_btn">ADD</button>
+      <button
+        onClick={() => {
+          Add_Product();
+        }}
+        className="addproduct_btn"
+      >
+        ADD
+      </button>
     </div>
   );
 };
--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -1,12 +1,53 @@
 // import React from 'react'
+import { useEffect, useState } from "react";
 import "./ListProduct.css";
+import croos_icon from "../../assets/Admin_Assets/cross_icon.png"
 
 const ListProduct = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [allproducts, setAllproducts] = useState([]);
 
-export default ListProduct
+  let api = "http://localhost:4000/allproducts";
+  const fetchInfo = async () => {
+    try {
+      const res = await fetch(api);
+      const data = await res.json();
+      console.log(data.products);
+      setAllproducts(data.products);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
+  return (
+    <div className="list_product">
+      <h1>All Products List</h1>
+      <div className="listproduct_format_main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Old Price</p>
+        <p>New Price</p>
+        <p>Category</p>
+        <p>Remove</p>
+      </div>
+      <div className="listproduct_allproducts">
+        <hr />
+        {allproducts.map((elem, i) => (
+          <div key={i} className="listproduct_format_main listproduct_format">
+            <img src={elem.image} alt={elem.name} className="listproduct_product_icon" />
+            <p>{elem.name}</p>
+            <p>${elem.old_price}</p>
+            <p>${elem.new_price}</p>
+            <p>{elem.category}</p>
+            <img src={croos_icon} className="list_product_remove_icon" alt="" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ListProduct;

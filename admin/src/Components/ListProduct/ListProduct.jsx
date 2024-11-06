2eb559589d7d312fceb40bf5824e27b9f9308816
unknown <mdamir4298@gmail.com>
2024-11-07 00:05:21 +0530

    signup and login created

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -1,7 +1,7 @@
 // import React from 'react'
 import { useEffect, useState } from "react";
 import "./ListProduct.css";
-import croos_icon from "../../assets/Admin_Assets/cross_icon.png"
+import croos_icon from "../../assets/Admin_Assets/cross_icon.png";
 
 const ListProduct = () => {
   const [allproducts, setAllproducts] = useState([]);
@@ -22,6 +22,23 @@ const ListProduct = () => {
     fetchInfo();
   }, []);
 
+  const remove_product = async (id) => {
+    try {
+      await fetch("http://localhost:4000/removeproduct", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id: id }),
+      });
+      await fetchInfo(); // Refresh the product list after removal
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+
   return (
     <div className="list_product">
       <h1>All Products List</h1>
@@ -36,14 +53,27 @@ const ListProduct = () => {
       <div className="listproduct_allproducts">
         <hr />
         {allproducts.map((elem, i) => (
-          <div key={i} className="listproduct_format_main listproduct_format">
-            <img src={elem.image} alt={elem.name} className="listproduct_product_icon" />
-            <p>{elem.name}</p>
-            <p>${elem.old_price}</p>
-            <p>${elem.new_price}</p>
-            <p>{elem.category}</p>
-            <img src={croos_icon} className="list_product_remove_icon" alt="" />
-          </div>
+          <>
+            {" "}
+            <div key={elem.id} className="listproduct_format_main listproduct_format">
+              <img
+                src={elem.image}
+                alt={elem.name}
+                className="listproduct_icon"
+              />
+              <p>{elem.name}</p>
+              <p>${elem.old_price}</p>
+              <p>${elem.new_price}</p>
+              <p>{elem.category}</p>
+              <img
+                onClick={()=>remove_product(elem.id)}
+                src={croos_icon}
+                className="list_product_remove_icon"
+                alt=""
+              />
+            </div>
+            <hr />
+          </>
         ))}
       </div>
     </div>

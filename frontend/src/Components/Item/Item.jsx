5c41e1be4dfe0b9f97bd384b180683e7508da56d
unknown <mdamir4298@gmail.com>
2024-11-09 18:55:35 +0530

    popular section added with css populars collections for women

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -7,8 +7,8 @@ const Item = (props) => {
       <img src={props.image} alt="" />
       <p>{props.name}</p>
       <div className="item_price">
-        <div className="item_price_new">{props.new_price}</div>
-        <div className="item_price_old">{props.old_price}</div>
+        <div className="item_price_new">$ {props.new_price}</div>
+        <div className="item_price_old">$ {props.old_price}</div>
       </div>
     </div>
   );

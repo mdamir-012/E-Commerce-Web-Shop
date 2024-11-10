63dac53aff02d229a269686c10d23e9f296dbb3d
unknown <mdamir4298@gmail.com>
2024-11-10 18:46:14 +0530

    loginsignup added with css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,23 +11,33 @@ import men_banner from "./Assets/Frontend_Assets/banner_mens.png";
 import women_banner from "./Assets/Frontend_Assets/banner_women.png";
 import kid_banner from "./Assets/Frontend_Assets/banner_kids.png";
 
-
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <Routes>
-            <Route path="/" element={<Shop/>}/>
-            <Route path="/mens" element={<ShopCategory banner={men_banner} category="men"/>}/>
-            <Route path="/womens" element={<ShopCategory banner={women_banner} category="women"/>}/>
-            <Route path="/kids" element={<ShopCategory banner={kid_banner} category="kid"/>}/>
-            <Route path="product" element={<Product/>}/>
-            <Route path=":productId" element={<Product/>}/>
-            <Route path="/cart" element={<Cart/>}/>
-            <Route path="/login" element={<LoginSignup/>}/>
+          <Route path="/" element={<Shop />} />
+          <Route
+            path="/mens"
+            element={<ShopCategory banner={men_banner} category="men" />}
+          />
+          <Route
+            path="/womens"
+            element={<ShopCategory banner={women_banner} category="women" />}
+          />
+          <Route
+            path="/kids"
+            element={<ShopCategory banner={kid_banner} category="kid" />}
+          />
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
+          </Route>
+
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
     </div>
   );

5ff0850294b079bb4dd8f7cafb6bda2f024d58ae
unknown <mdamir4298@gmail.com>
2024-11-10 17:59:40 +0530

    shopbycategory section added

---
+++ frontend/src/Pages/LoginSignup.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const LoginSignup = () => {
+  return (
+    <div className="loginsignup">
+      <div className="loginsignup_container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup_fields">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup_login">
+          Already have ab account? <span>Login</span>
+          <div className="loginsignup_agree">
+            <input type="checkbox" name="" id="" />
+            <p>By continuing, i agree to use & privacy policy</p>
+          </div>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default LoginSignup;
--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useContext } from "react";
+import "./CSS/ShopCategory.css";
+import { ShopContext } from "../Context/ShopContext";
+import dropdown_icon from "../Assets/Frontend_Assets/dropdown_icon.png";
+import Item from "../Components/Item/Item";
+const ShopCategory = (props) => {
+  const { all_product } = useContext(ShopContext);
 
-const ShopCategory = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="shop_category">
+      <img src={props.banner} alt="" className="shopcategory_banner" />
+      <div className="shopcategory_indexsort">
+        <p>
+          <span>Showing 1-12</span> out of 36 products
+        </p>
+        <div className="shopcategory_sort">
+          Sort by <img src="" alt="" />
+        </div>
+      </div>
+      <div className="shopcategory_products">
+        {all_product.map((item, i) => {
+          if (props.category === item.category) {
+            return <Item key={item.id} {...item} />;
+          }else{
+            return null
+          }
+        })}
+      </div>
+      <div className="shopcategory_loadmore">
+      Explore More 
 
-export default ShopCategory
+      </div>
+    </div>
+  );
+};
+
+export default ShopCategory;
